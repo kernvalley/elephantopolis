ffc3bb36336af47c63c610c1e587ff236912ec10
Chris Zuber <shgysk8zer0@gmail.com>
2023-05-17 18:00:07 -0700

    Fix share button

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 if (navigator.share instanceof Function) {
 	document.querySelectorAll('.btn.share').forEach(btn => {
 		btn.addEventListener('click', ({ currentTarget }) => {
-			navigator.share(currentTarget.dataset);
+			const { title = document.title, url = location.href, text } = currentTarget.dataset;
+			navigator.share({ title, text, url });
 		});
 
 		btn.disabled = false;
